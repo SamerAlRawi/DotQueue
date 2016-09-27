58fffed1066bb158e7959b64cbc95687ca79acf2
saa133 <samer.alrawi@sherwin.com>
2016-09-27 10:56:55 -0400

    Init

---
+++ DotQueue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+using Topshelf;
+
+namespace DotQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Queue>(s =>
+                {
+                    s.ConstructUsing(name => new Queue());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("DotQueue Host");
+                x.SetDisplayName("DotQueue");
+                x.SetServiceName("DotQueue");
+            });
+        }
+    }
+
+    public class Queue
+    {
+        readonly Timer _timer;
+        public Queue()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+
+}
