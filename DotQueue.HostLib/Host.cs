dadd96dec1976f3150008ed107eea2f106b1666f
Samer <samer_control@yahoo.com>
2016-09-27 21:57:02 -0400

    moved host files to new library

---
+++ DotQueue.HostLib/Host.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+namespace DotQueue.HostLib
+{
+    public class HostParameters
+    {
+        public long ApiPort { get; set; }
+        public long DashboardPort { get; set; }
+    }
+
+    public class Host
+    {
+        private HttpSelfHostServer _httpSelfHostServer;
+        private IDisposable _staticFileHost;
+        private HostParameters _parameters;
+
+        public Host(HostParameters parameters)
+        {
+            _parameters = parameters;
+        }
+
+        public void Start()
+        {
+            StartApiHost();
+            StartFileHost();
+        }
+
+        private void StartFileHost()
+        {
+            var url = $"http://*:{_parameters.DashboardPort}/";
+            var fileSystem = new PhysicalFileSystem("");
+            var options = new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                FileSystem = fileSystem
+            };
+            options.StaticFileOptions.ContentTypeProvider = new CustomContentTypeProvider();
+            _staticFileHost = WebApp.Start(url, builder => builder.UseFileServer(options));
+        }
+
+        private void StartApiHost()
+        {
+            HttpSelfHostConfiguration _configuration = new HttpSelfHostConfiguration($"http://0.0.0.0:{_parameters.ApiPort}");
+            _configuration.Routes.MapHttpRoute("ApiDefault", "api/{controller}/{method}/{id}",
+                new { id = RouteParameter.Optional });
+            _httpSelfHostServer = new HttpSelfHostServer(_configuration);
+            _httpSelfHostServer.OpenAsync().Wait();
+        }
+
+        public void Stop()
+        {
+            _httpSelfHostServer.CloseAsync();
+            _httpSelfHostServer.Dispose();
+            _staticFileHost.Dispose();
+        }
+    }
+}
--- DotQueue/Program.cs
+++ DotQueue/Program.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Reflection;
+﻿using DotQueue.HostLib;
 using Topshelf;
-using System.Web.Http;
-using System.Web.Http.SelfHost;
-using Microsoft.Owin.StaticFiles;
-using Owin;
-using Microsoft.Owin.FileSystems;
-using Microsoft.Owin.Hosting;
-using Microsoft.Owin.StaticFiles.ContentTypes;
+using Host = DotQueue.HostLib.Host;
 
 namespace DotQueue
 {
     class Program
     {
+        private static int _apiPort = 8083;
+        private static int _dashboardPort = 8082;
+
         static void Main(string[] args)
         {
             HostFactory.Run(x =>
             {
                 x.Service<Host>(s =>
                 {
-                    s.ConstructUsing(name => new Host());
+                    s.ConstructUsing(name =>
+                    {
+                        var hostParameters = new HostParameters { ApiPort = _apiPort, DashboardPort = _dashboardPort };
+                        return new Host(hostParameters);
+                    });
                     s.WhenStarted(tc => tc.Start());
                     s.WhenStopped(tc => tc.Stop());
                 });
@@ -29,55 +29,6 @@ namespace DotQueue
                 x.SetDisplayName("DotQueue");
                 x.SetServiceName("DotQueue");
             });
-        }
-    }
-
-    public class Host
-    {
-        private HttpSelfHostServer _httpSelfHostServer;
-        private IDisposable _staticFileHost;
-
-        public void Start()
-        {
-            StartApiHost();
-            StartFileHost();
-        }
-
-        private void StartFileHost()
-        {
-            var url = "http://*:8082/";
-            var fileSystem = new PhysicalFileSystem("");
-            var options = new FileServerOptions
-            {
-                EnableDirectoryBrowsing = true,
-                FileSystem = fileSystem
-            };
-            options.StaticFileOptions.ContentTypeProvider = new CustomContentTypeProvider();
-            _staticFileHost = WebApp.Start(url, builder => builder.UseFileServer(options));
-        }
-
-        private void StartApiHost()
-        {
-            HttpSelfHostConfiguration _configuration = new HttpSelfHostConfiguration("http://0.0.0.0:8080");
-            _configuration.Routes.MapHttpRoute("ApiDefault", "api/{controller}/{method}/{id}",
-                new { id = RouteParameter.Optional });
-            _httpSelfHostServer = new HttpSelfHostServer(_configuration);
-            _httpSelfHostServer.OpenAsync().Wait();
-        }
-
-        public void Stop()
-        {
-            _httpSelfHostServer.CloseAsync();
-            _httpSelfHostServer.Dispose();
-
-            _staticFileHost.Dispose();
-        }
-    }
-    public class CustomContentTypeProvider : FileExtensionContentTypeProvider
-    {
-        public CustomContentTypeProvider()
-        {
-            //Mappings.Add(".js", "text/javascript");
         }
     }
 }
