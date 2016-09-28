1806c7a859a2e334c849ecb05ca97735e79eea3f
saa133 <samer.alrawi@sherwin.com>
2016-09-28 12:56:56 -0400

    moved controllers to api project.

--- DotQueue.Api/MainController.cs
+++ DotQueue.Api/MainController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Web.Http;
 
-namespace MyNamespace
+namespace DotQueue.Api
 {
     public class MainController : ApiController
     {
--- DotQueue.Api/Message.cs
+++ DotQueue.Api/Message.cs
@@ -1,4 +1,4 @@
-﻿namespace MyNamespace
+﻿namespace DotQueue.Api
 {
     public class Message
     {
