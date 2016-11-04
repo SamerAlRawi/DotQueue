eb0db9cc844ede2b6e7885b15e97d83d176af518
Samer <samer_control@yahoo.com>
2016-11-03 21:06:59 -0400

    Extracted HttpAdapter

--- DotQueue.Client/MessageQueue.cs
+++ DotQueue.Client/MessageQueue.cs
@@ -18,12 +18,26 @@ namespace DotQueue.Client
         private bool _messageFound = false;
         private bool _subscribed = false;
         private DateTime _subscriptionConfirmTime = DateTime.MinValue;
+        private IHttpAdapter<T> _httpAdapter;
 
         public MessageQueue(DotQueueAddress address)
         {
+            _httpAdapter = new HttpAdapter<T>(address);
             _address = address;
             _type = typeof(T).Name;
+            InitializeQueueTasks();
+        }
 
+        internal MessageQueue(DotQueueAddress address, IHttpAdapter<T> httpAdapter)
+        {
+            _httpAdapter = httpAdapter;
+            _address = address;
+            _type = typeof(T).Name;
+            InitializeQueueTasks();
+        }
+
+        private void InitializeQueueTasks()
+        {
             Task.Run(() => StartListener());
             Task.Run(() => SubscribeToQueue(_localPort));
             Task.Run(() => ReSubscribe());
@@ -87,96 +101,17 @@ namespace DotQueue.Client
 
         public string Add(T message)
         {
-            var postData = BuildMessage(message);
-            var request = BuildAddHttpRequest();
-            SetHeaders(request, WebRequestMethods.Http.Post);
-            PostMessage(request, postData);
-            return GetResponseFromServer(request);
+            return _httpAdapter.Add(message);
         }
 
         public T Pull()
         {
-            var request = BuildPullHttpRequest();
-            SetHeaders(request, WebRequestMethods.Http.Get);
-            var json = GetResponseFromServer(request);
-            return JsonConvert.DeserializeObject<T>(json);
+            return _httpAdapter.Pull();
         }
 
         public int Count()
         {
-            var request = BuildCountHttpRequest();
-            SetHeaders(request, WebRequestMethods.Http.Get);
-            var json = GetResponseFromServer(request);
-            return int.Parse(json);
-        }
-
-        private HttpWebRequest BuildPullHttpRequest()
-        {
-            var request =
-                WebRequest.Create($"http://{_address.IpAddress}:{_address.Port}/api/Queue/Pull?category={_type}") as
-                    HttpWebRequest;
-            return request;
-        }
-
-        private HttpWebRequest BuildCountHttpRequest()
-        {
-            var requestUriString = $"http://{_address.IpAddress}:{_address.Port}/api/Queue/Count?category={_type}";
-            var request = WebRequest.Create(requestUriString) as HttpWebRequest;
-            return request;
-        }
-
-        private HttpWebRequest BuildSubscribeHttpRequest(int port)
-        {
-            var requestUriString =
-                $"http://{_address.IpAddress}:{_address.Port}/api/Subscribe/Subscribe?category={_type}&port={port}";
-            var request = WebRequest.Create(requestUriString) as HttpWebRequest;
-            return request;
-        }
-
-        private HttpWebRequest BuildAddHttpRequest()
-        {
-            var request =
-                WebRequest.Create($"http://{_address.IpAddress}:{_address.Port}/api/Queue/Add") as HttpWebRequest;
-            return request;
-        }
-
-        private string BuildMessage(T message)
-        {
-            var msg = JsonConvert.SerializeObject(message);
-            var wrapper = new Message {Type = _type, Body = msg};
-            var postData = JsonConvert.SerializeObject(wrapper);
-            return postData;
-        }
-
-        private static void PostMessage(HttpWebRequest request, string postData)
-        {
-            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-            request.ContentLength = byteArray.Length;
-            using (Stream dataStream = request.GetRequestStream())
-            {
-                dataStream.Write(byteArray, 0, byteArray.Length);
-            }
-        }
-
-        private static string GetResponseFromServer(HttpWebRequest request)
-        {
-            string responseFromServer;
-            using (WebResponse response = request.GetResponse())
-            {
-                using (Stream stream = response.GetResponseStream())
-                {
-                    StreamReader reader = new StreamReader(stream);
-                    responseFromServer = reader.ReadToEnd();
-                }
-            }
-            return responseFromServer;
-        }
-
-        private static void SetHeaders(HttpWebRequest request, string method)
-        {
-            request.Method = method;
-            request.ContentType = "application/json";
-            request.Accept = "application/json";
+            return _httpAdapter.Count();
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -208,17 +143,9 @@ namespace DotQueue.Client
             }
         }
 
-        private void SubscribeToQueue(int localPort)
+        private bool SubscribeToQueue(int localPort)
         {
-            try
-            {
-                var request = BuildSubscribeHttpRequest(localPort);
-                request.Method = WebRequestMethods.Http.Get;
-                request.GetResponse();
-            }
-            catch (Exception)
-            {
-            }
+            return _httpAdapter.Subscribe(localPort);
         }
     }
 }
