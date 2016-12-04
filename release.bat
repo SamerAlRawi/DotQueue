5d7eee3fe5aac9b2ecff9b4aa4123dc35fa693df
Samer <samer_control@yahoo.com>
2016-12-04 11:57:31 -0500

    release.bat build and run unit tests before packaging nupkg files

--- release.bat
+++ release.bat
@@ -1,4 +1,23 @@
 @ECHO OFF
+
+MSBuild.exe DotQueue.sln /t:Build /p:Configuration=Release
+if /I "%ERRORLEVEL%" NEQ "0" (
+   echo MSBUILD Task failed %ERRORLEVEL%
+   exit /b %ERRORLEVEL%
+)
+
+nunit3-console.exe ".\DotQueue.HostLib.Tests\bin\Release\DotQueue.HostLib.Tests.dll" --output=result.xml --where "cat != no_ci"
+if /I "%ERRORLEVEL%" NEQ "0" (
+   echo NUNIT Task failed %ERRORLEVEL%
+   exit /b %ERRORLEVEL%
+)
+
+nunit3-console.exe ".\DotQueue.Client.Tests\bin\Release\DotQueue.Client.Tests.dll" --output=result.xml --where "cat != no_ci"
+if /I "%ERRORLEVEL%" NEQ "0" (
+   echo NUNIT Task failed %ERRORLEVEL%
+   exit /b %ERRORLEVEL%
+)
+
 :Loop
 IF "%1"=="" GOTO Continue
    nuget pack DotQueue.Client.nuspec -version %1
